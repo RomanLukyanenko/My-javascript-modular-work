204a7670d9e6f31897fd45aa31f39fdbbebbdffc
Roman Lukianenko <142048423+RomanLukyanenko@users.noreply.github.com>
2024-01-03 01:18:16 +0200

    Update script.js

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -1,4 +1,4 @@
-const resultField = document.getElementById("result");
+const resultField = document.getElementById("result-input");
 
 function append(value) {
     resultField.value += value;
@@ -13,49 +13,25 @@ function appendMathFunction(funcName) {
 }
 
 function calculate() {
-    let expression = resultField.value;
-
-    expression = expression.replace(/\^/g, "**");
-    
     try {
-        expression = expression
-            .replace(/sqrt\(([^)]+)\)/g, (_, match) => Math.sqrt(parseFloat(match)))
-            .replace(/sin\(([^)]+)\)/g, (_, match) => Math.sin(parseFloat(match)))
-            .replace(/cos\(([^)]+)\)/g, (_, match) => Math.cos(parseFloat(match)))
-            .replace(/tan\(([^)]+)\)/g, (_, match) => Math.tan(parseFloat(match)));
-
-        resultField.value = eval(expression);
+        resultField.value = math.evaluate(resultField.value);
     } catch (e) {
         resultField.value = "Помилка";
     }
 }
 
 document.addEventListener("keydown", function(event) {
-switch (event.key) {
-    case '1': case '2': case '3': case '4': case '5':
-    case '6': case '7': case '8': case '9': case '0':
-        append(event.key);
-        break;
-    case '+':
-        append('+');
-        break;
-    case '-':
-        append('-');
-        break;
-    case '*':
-        append('*');
-        break;
-    case '/':
-        append('/');
-        break;
-    case '=': case 'Enter':
-        calculate();
-        break;
-    case 'Escape':
-        clearInput();
-        break;
-    case '^':
-        append('^');
-        break;
-}
+    switch (event.key) {
+        case '1': case '2': case '3': case '4': case '5':
+        case '6': case '7': case '8': case '9': case '0':
+        case '+': case '-': case '*': case '/': case '^':
+            append(event.key);
+            break;
+        case '=': case 'Enter':
+            calculate();
+            break;
+        case 'Escape':
+            clearInput();
+            break;
+    }
 });
